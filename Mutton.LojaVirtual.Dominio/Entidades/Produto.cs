a2f6e32d74d1b7bcf132b82dc431bc077e732cb4
matheusmutton <matheus.mutton@gmail.com>
2015-08-28 18:24:25 -0300

    Aula 5 e 6 (EF + DB)

--- Mutton.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Mutton.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -8,7 +8,7 @@ namespace Mutton.LojaVirtual.Dominio.Entidades
 {
     public class Produto
     {
-        public int Id { get; set; }
+        public int ProdutoId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
---
+++ Mutton.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mutton.LojaVirtual.Dominio.Entidades;
+using Mutton.LojaVirtual.Dominio.Repositorio;
+
+namespace Mutton.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutosRepositorio _reposito;
+
+        //
+        // GET: /Produto/
+        public ActionResult Index()
+        {
+            _reposito = new ProdutosRepositorio();
+            
+            //var produtos = _reposito.Produtos.Take(10);
+            List<Produto> produtos = _reposito.Produtos.Take(10).ToList();
+            
+            return View(produtos);
+        }
+	}
+}
